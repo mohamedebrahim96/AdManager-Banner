877f1a18a9fbe58e9bc9a1d63189894f0202c642
mohamedebrahim96 <ebrahimm131@gmail.com>
2019-03-07 12:00:28 +0200

    change size and unit id dynamclly

--- app/src/main/java/com/google/android/gms/example/bannerexample/MyActivity.java
+++ app/src/main/java/com/google/android/gms/example/bannerexample/MyActivity.java
@@ -17,6 +17,9 @@ package com.google.android.gms.example.bannerexample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
 import com.google.android.gms.ads.doubleclick.PublisherAdView;
 
@@ -34,8 +37,11 @@ public class MyActivity extends AppCompatActivity {
 
         // Gets the ad view defined in layout/ad_fragment.xml with ad unit ID set in
         // values/strings.xml.
-        adView = findViewById(R.id.ad_view);
-
+        LinearLayout v = findViewById(R.id.ad_view);
+        adView = new PublisherAdView(this);
+        adView.setAdSizes(AdSize.MEDIUM_RECTANGLE);
+        adView.setAdUnitId(getString(R.string.ad_unit_id));
+        v.addView(adView);
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
